3a0772cb2d492291d94c4693adab192c3bda7faf
uanoob <usnet@ukr.net>
2018-09-17 18:09:04 +0300

    Add feature -> open-close sandwich menu

--- source/js/app.js
+++ source/js/app.js
@@ -1,1 +1,18 @@
-console.log("js ok");
+const menuBtn = document.querySelector('.main-header__button-menu');
+const navbar = document.querySelector('.navbar');
+
+menuBtn.addEventListener('click', function() {
+  openMenu();
+});
+
+function openMenu() {
+  navbar.classList.toggle('navbar__hide');
+}
+
+function closeMenu() {
+  navbar.classList.add('navbar__hide');
+}
+
+window.addEventListener('DOMContentLoaded', function() {
+  closeMenu();
+});
