d808fad4f38757e4f58850e62bda1278ff15d6eb
uanoob <usnet@ukr.net>
2018-09-21 11:02:33 +0300

    Add overlay & modal handler functions

--- source/js/app.js
+++ source/js/app.js
@@ -1,3 +1,4 @@
+//open menu
 const menuBtn = document.querySelector('.main-header__button-menu');
 const navbar = document.querySelector('.navbar');
 
@@ -16,3 +17,46 @@ function closeMenu() {
 window.addEventListener('DOMContentLoaded', function() {
   closeMenu();
 });
+
+// popup
+const showModalBtns = document.querySelectorAll('.products__button--calc');
+const overlay = document.querySelector('.overlay');
+const modal = document.querySelector('.modal-feedback');
+const closeModalBtn = document.querySelector('.modal-feedback__close');
+
+showModalBtns.forEach(btn => {
+  btn.addEventListener('click', function() {
+    showModal(event);
+  });
+});
+
+closeModalBtn.addEventListener('click', function() {
+  closeModal(event);
+});
+
+function showModal(event) {
+  event.preventDefault();
+  overlay.classList.add('overlay-show');
+  modal.classList.add('modal-feedback__show');
+}
+
+function closeModal(event) {
+  event.preventDefault();
+  overlay.classList.remove('overlay-show');
+  modal.classList.remove('modal-feedback__show');
+}
+
+//send data
+const submitIntroBtn = document.querySelector('.intro__button');
+const submitModalBtn = document.querySelector('.modal-feedback__button');
+
+submitIntroBtn.addEventListener('click', function() {
+  sendData(event);
+});
+submitModalBtn.addEventListener('click', function() {
+  sendData(event);
+});
+
+function sendData(event) {
+  event.preventDefault();
+}
